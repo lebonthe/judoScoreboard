758834644474441928620b517402373d7da75efb
Min Hu <lebonthe@gmail.com>
2023-09-26 16:11:42 +0800

    Initial Commit

---
+++ judoScoreboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  judoScoreboard
+//
+//  Created by Min Hu on 2023/9/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
